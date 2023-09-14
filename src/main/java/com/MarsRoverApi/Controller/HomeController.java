790e05767c8b865ff58956d3bcc97a8a2c0ca606
jose <josefilmeneses@gmail.com>
2023-09-14 16:14:14 +0100

    mars photos now can be displayed

--- src/main/java/com/MarsRoverApi/Controller/HomeController.java
+++ src/main/java/com/MarsRoverApi/Controller/HomeController.java
@@ -1,15 +1,21 @@
 package com.MarsRoverApi.Controller;
 
+import com.MarsRoverApi.Response.MarsRoverApiResponse;
+import com.MarsRoverApi.Service.MarsRoverApiService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+@Autowired
+private MarsRoverApiService roverApiService;
 
     @GetMapping("/")
     public String InitialHome(Model model) {
-        model.addAttribute("name", "josePage");
+        MarsRoverApiResponse roverData = roverApiService.getRoverData();
+        model.addAttribute("roverData", roverData);
         return "index";
     }
 
