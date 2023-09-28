1e7b16b2cae44ecc5cd6d23c33f980ee33d48f33
jose <josefilmeneses@gmail.com>
2023-09-28 23:45:41 +0100

    more checkBoxes

--- src/main/java/com/MarsRoverApi/DTO/HomeDto.java
+++ src/main/java/com/MarsRoverApi/DTO/HomeDto.java
@@ -9,5 +9,14 @@ public class HomeDto {
 
     private String marsRoverApi;
     private Integer marsSol;
-    private Boolean defaultCheck1;
+    private Boolean cameraFhaz;
+    private Boolean cameraRhaz;
+    private Boolean cameraMast;
+    private Boolean cameraChemcam;
+    private Boolean cameraMahli;
+    private Boolean cameraMardi;
+    private Boolean cameraNavcam;
+    private Boolean cameraPancam;
+    private Boolean cameraMinites;
+
 }
